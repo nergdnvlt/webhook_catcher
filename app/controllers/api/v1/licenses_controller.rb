469cac8ba9342e8bcb3ad0397e0aca2cfc39d203
Tyler Lundgren <tylerlundgren@yahoo.com>
2019-01-03 11:52:52 -0700

    Building listener for license.

--- app/controllers/api/v1/licenses_controller.rb
+++ app/controllers/api/v1/licenses_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::LicensesController < ApiController
   def create
-    license = LicenseService.create(params['events'][0])
+    license = LicenseService.create(params)
     render json: license, status: 200
   end
 end
