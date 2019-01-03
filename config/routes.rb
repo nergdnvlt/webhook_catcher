61787d5eb15142be22912874414df9904a97606f
Tyler Lundgren <tylerlundgren@yahoo.com>
2019-01-03 11:56:44 -0700

    That's what happens when you don't test -> needed to wire together in routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   resources :payouts, only: [:index]
+  resources :licenses, only: [:index]
 
   namespace :api do
     namespace :v1 do
       post 'payouts', to: 'payouts#create'
+      post 'licenses', to: 'licenses#create'
     end
   end
 end
