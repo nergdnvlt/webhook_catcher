194c736a1a02aad9b60754bef953a1c17c06b300
Tyler Lundgren <tylerlundgren@yahoo.com>
2019-01-03 15:45:53 -0700

    Refining info inside monthly subscriptions.

--- app/controllers/api/v1/subs/monthly_subscriptions_controller.rb
+++ app/controllers/api/v1/subs/monthly_subscriptions_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::Subs::MonthlySubscriptionsController < ApiController
   def create
     sub = MonthlySubService.create(params)
-    render json: license.as_json, status: 200
+    render json: sub, status: 200
   end
 end
--- app/services/monthly_sub_service.rb
+++ app/services/monthly_sub_service.rb
@@ -1,13 +1,20 @@
-class MonthlyLicenseService
+class MonthlySubService
   def self.create(attrs)
     new(attrs).create
   end
 
   def create
-    MonthlySubscription.create!(data: @data)
+    MonthlySubscription.create!(data: @data, sub_id: @sub_id)
   end
 
   def initialize(attrs)
-    @data = attrs
+    @data = {
+      order_id: attrs['order_id'],
+      quantity: attrs['quantity'],
+      internalProductName: attrs['internalProductName'],
+      security_request_hash: attrs['security_request_hash'],
+      subscriptionReference: attrs['subscriptionReference'],
+    }
+    @sub_id = attrs['subscriptionReference']
   end
 end
