e6470f0b70610a11bbf23b71bf51f427e9095fe3
Tyler Lundgren <tylerlundgren@yahoo.com>
2019-04-01 13:51:47 -0600

    Trying to update license.

--- app/controllers/api/v1/monthly_licenses_controller.rb
+++ app/controllers/api/v1/monthly_licenses_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::MonthlyLicensesController < ApiController
   def create
     license = MonthlyLicenseService.create(params)
-    render json: license, status: 200
+    render json: license.as_json, status: 200
   end
 end
